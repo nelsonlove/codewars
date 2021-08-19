7147a3caa40f00e29eb820b8c7ed4dd2e886b7c3
nelsonlove <nelson.love@me.com>
2021-08-19 14:30:15 -0400

    Removed multiply.py, no_space.py

--- kata/__init__.py
+++ kata/__init__.py
@@ -17,20 +17,19 @@ from calculating_with_functions import (
     times,
     divided_by
 )
-from validate_sudoku import validate_sudoku
+
 from duplicate_count import duplicate_count
 from duplicate_encode import duplicate_encode
 from filter_list import filter_list
+from find_local_peaks import find_local_peaks
 from find_outlier import find_outlier
 from get_PIN_variations import get_PIN_variations
 from is_pangram import is_pangram
 from is_valid_walk import is_valid_walk
 from make_readable import make_readable
 from max_sequence import max_sequence
-from multiply import multiply
-from no_space import no_space
 from PaginationHelper import PaginationHelper
-from find_local_peaks import find_local_peaks
 from rot13_cipher_encode import rot13_cipher_encode
 from strip_comments import strip_comments
 from unique_in_order import unique_in_order
+from validate_sudoku import validate_sudoku
