469c0c4fce4514349bbc2f3f44006758cc65b228
nelsonlove <nelson.love@me.com>
2021-08-19 12:36:09 -0400

    Added "duplicate_encode"

--- kata/__init__.py
+++ kata/__init__.py
@@ -4,3 +4,4 @@ from filter_list import filter_list
 from unique_in_order import unique_in_order
 from find_outlier import find_outlier
 from make_readable import make_readable
+from duplicate_encode import duplicate_encode
---
+++ kata/tests/test_duplicate_encode.py
@@ -0,0 +1,34 @@
+from random import randint
+from unittest import TestCase
+
+from kata import duplicate_encode
+
+
+class Test(TestCase):
+    def test_duplicate_encode(self):
+        self.assertEqual(duplicate_encode("din"), "(((")
+        self.assertEqual(duplicate_encode("recede"), "()()()")
+        self.assertEqual(duplicate_encode("Success"), ")())())", "should ignore case")
+        self.assertEqual(duplicate_encode("CodeWarrior"), "()(((())())")
+        self.assertEqual(duplicate_encode("Supralapsarian"), ")()))()))))()(", "should ignore case")
+        self.assertEqual(duplicate_encode("iiiiii"), "))))))", "duplicate-only-string")
+
+        # Tests with '(' and ')'
+        self.assertEqual(duplicate_encode("(( @"), "))((")
+        self.assertEqual(duplicate_encode(" ( ( )"), ")))))(")
+
+        # And now... some random tests !
+
+        def duplicate_sol(word):
+            return "".join(["(" if word.lower().count(x.lower()) == 1 else ")" for x in word])
+
+        for _ in range(40):
+            testlen = randint(6, 40)
+            testword = ""
+            for i in range(testlen):
+                letter = "abcdefghijklmnopqrstuvwxyz() @!"[randint(0, 30)]
+                testword += letter if randint(0, 1) == 0 else letter.upper()
+            self.assertEqual(duplicate_encode(testword),
+                             duplicate_sol(testword),
+                             "It Should encode '" + duplicate_sol(testword) + "'"
+                             )
